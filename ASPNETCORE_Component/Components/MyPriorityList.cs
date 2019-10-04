970ba368375e2c633f1c399f32c0e78d174a5c5d
vijay113 <vijay113kumar@gmail.com>
2019-10-04 12:59:23 +0530

    Init repo

---
+++ ASPNETCORE_Component/Components/MyPriorityList.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNETCORE_Component.Components
+{
+    // 3 way to define viewcomponent class:--->
+    //public class MyPriorityListViewComponent : ViewComponent
+    //public class MyPriorityList : ViewComponent
+
+    // [ViewComponent(Name = "CustomNameOfMyPriorityList")]   // We can change name of Component using attribute "ViewComponent"
+    public class MyPriorityList : ViewComponent
+    {
+
+
+        public MyPriorityList()   // Can Use Dependency Injection 
+        {
+        
+        }
+
+        public IViewComponentResult Invoke(int ItemCount)
+        {
+            string viewName = "Default";
+            if (ItemCount > 4)
+            {
+                viewName = "MyViewComp";
+            }
+            var items = GetItemsAsync(ItemCount);
+            return View(viewName,items);
+        }
+
+        private List<string> GetItemsAsync(int ItemCount)
+        {
+
+            List<string> items = new List<string>() { "Item1","Item2","Item3","Item4","Item5","Item6"};
+            var LimitedItems= items.Take(ItemCount).ToList();
+            return LimitedItems;
+        }
+        
+    }
+}
---
+++ ASPNETCORE_Component/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ASPNETCORE_Component.Models;
+
+namespace ASPNETCORE_Component.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index(int Id=3)
+        {          
+            //We can Call ViewComponent directly from Controller using following code:-
+            //return ViewComponent("CustomNameOfMyPriorityList",new { ItemCount=Id});
+            //return ViewComponent("MyPriorityList", new { ItemCount=Id });           
+            
+             return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
